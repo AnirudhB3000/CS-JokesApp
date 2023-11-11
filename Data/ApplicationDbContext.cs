349b9db9720f678ebcf7b04835af68f0f4136175
Anirudh Bhattacharya <anirudh.b@somaiya.edu>
2023-11-10 19:35:31 -0800

    init

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Joks.Models;
+
+namespace Joks.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Joks.Models.Joke>? Joke { get; set; }
+    }
+}
---
+++ Models/Joke.cs
@@ -0,0 +1,13 @@
+﻿namespace Joks.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke() { 
+        
+        }
+    }
+}
